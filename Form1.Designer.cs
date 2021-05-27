759c43e3e971a93b5f954580eaa1419ba6750022
lycera8 <mraycel@gmail.com>
2021-05-28 02:48:41 +0800

    added buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,26 +29,358 @@ namespace Calculator_Proj_Manalo
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.num1 = new System.Windows.Forms.Button();
+            this.numBox = new System.Windows.Forms.TextBox();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.negate = new System.Windows.Forms.Button();
+            this.period = new System.Windows.Forms.Button();
+            this.num0 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.equalSign = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.clearEachNum = new System.Windows.Forms.Button();
+            this.clearAll = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.AccessibleName = "";
+            this.num1.BackColor = System.Drawing.Color.MediumPurple;
+            this.num1.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.num1.ForeColor = System.Drawing.Color.Black;
+            this.num1.Location = new System.Drawing.Point(12, 353);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(104, 69);
+            this.num1.TabIndex = 0;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = false;
+            // 
+            // numBox
+            // 
+            this.numBox.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.numBox.Font = new System.Drawing.Font("Baskerville Old Face", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numBox.Location = new System.Drawing.Point(12, 12);
+            this.numBox.Multiline = true;
+            this.numBox.Name = "numBox";
+            this.numBox.Size = new System.Drawing.Size(434, 99);
+            this.numBox.TabIndex = 1;
+            // 
+            // num7
+            // 
+            this.num7.AccessibleName = "";
+            this.num7.BackColor = System.Drawing.Color.MediumPurple;
+            this.num7.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.num7.ForeColor = System.Drawing.Color.Black;
+            this.num7.Location = new System.Drawing.Point(12, 203);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(104, 69);
+            this.num7.TabIndex = 2;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = false;
+            // 
+            // num6
+            // 
+            this.num6.AccessibleName = "";
+            this.num6.BackColor = System.Drawing.Color.MediumPurple;
+            this.num6.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.num6.ForeColor = System.Drawing.Color.Black;
+            this.num6.Location = new System.Drawing.Point(232, 278);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(104, 69);
+            this.num6.TabIndex = 3;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = false;
+            // 
+            // num5
+            // 
+            this.num5.AccessibleName = "";
+            this.num5.BackColor = System.Drawing.Color.MediumPurple;
+            this.num5.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.num5.ForeColor = System.Drawing.Color.Black;
+            this.num5.Location = new System.Drawing.Point(122, 278);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(104, 69);
+            this.num5.TabIndex = 4;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = false;
+            // 
+            // num4
+            // 
+            this.num4.AccessibleName = "";
+            this.num4.BackColor = System.Drawing.Color.MediumPurple;
+            this.num4.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.num4.ForeColor = System.Drawing.Color.Black;
+            this.num4.Location = new System.Drawing.Point(12, 278);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(104, 69);
+            this.num4.TabIndex = 5;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = false;
+            // 
+            // negate
+            // 
+            this.negate.AccessibleName = "";
+            this.negate.BackColor = System.Drawing.Color.MediumPurple;
+            this.negate.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.negate.ForeColor = System.Drawing.Color.Black;
+            this.negate.Location = new System.Drawing.Point(12, 128);
+            this.negate.Name = "negate";
+            this.negate.Size = new System.Drawing.Size(104, 69);
+            this.negate.TabIndex = 6;
+            this.negate.Text = "+/-";
+            this.negate.UseVisualStyleBackColor = false;
+            // 
+            // period
+            // 
+            this.period.AccessibleName = "";
+            this.period.BackColor = System.Drawing.Color.MediumPurple;
+            this.period.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.period.ForeColor = System.Drawing.Color.Black;
+            this.period.Location = new System.Drawing.Point(12, 428);
+            this.period.Name = "period";
+            this.period.Size = new System.Drawing.Size(104, 69);
+            this.period.TabIndex = 7;
+            this.period.Text = ".";
+            this.period.UseVisualStyleBackColor = false;
+            this.period.Click += new System.EventHandler(this.period_Click);
+            // 
+            // num0
+            // 
+            this.num0.AccessibleName = "";
+            this.num0.BackColor = System.Drawing.Color.MediumPurple;
+            this.num0.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.num0.ForeColor = System.Drawing.Color.Black;
+            this.num0.Location = new System.Drawing.Point(122, 428);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(104, 69);
+            this.num0.TabIndex = 8;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = false;
+            this.num0.Click += new System.EventHandler(this.num0_Click);
+            // 
+            // num3
+            // 
+            this.num3.AccessibleName = "";
+            this.num3.BackColor = System.Drawing.Color.MediumPurple;
+            this.num3.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.num3.ForeColor = System.Drawing.Color.Black;
+            this.num3.Location = new System.Drawing.Point(232, 353);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(104, 69);
+            this.num3.TabIndex = 9;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = false;
+            // 
+            // num2
+            // 
+            this.num2.AccessibleName = "";
+            this.num2.BackColor = System.Drawing.Color.MediumPurple;
+            this.num2.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.num2.ForeColor = System.Drawing.Color.Black;
+            this.num2.Location = new System.Drawing.Point(122, 353);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(104, 69);
+            this.num2.TabIndex = 10;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = false;
+            this.num2.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // equalSign
+            // 
+            this.equalSign.AccessibleName = "";
+            this.equalSign.BackColor = System.Drawing.Color.MediumPurple;
+            this.equalSign.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.equalSign.ForeColor = System.Drawing.Color.Black;
+            this.equalSign.Location = new System.Drawing.Point(232, 428);
+            this.equalSign.Name = "equalSign";
+            this.equalSign.Size = new System.Drawing.Size(214, 69);
+            this.equalSign.TabIndex = 11;
+            this.equalSign.Text = "=";
+            this.equalSign.UseVisualStyleBackColor = false;
+            // 
+            // num9
+            // 
+            this.num9.AccessibleName = "";
+            this.num9.BackColor = System.Drawing.Color.MediumPurple;
+            this.num9.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.num9.ForeColor = System.Drawing.Color.Black;
+            this.num9.Location = new System.Drawing.Point(232, 203);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(104, 69);
+            this.num9.TabIndex = 12;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = false;
+            // 
+            // num8
+            // 
+            this.num8.AccessibleName = "";
+            this.num8.BackColor = System.Drawing.Color.MediumPurple;
+            this.num8.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.num8.ForeColor = System.Drawing.Color.Black;
+            this.num8.Location = new System.Drawing.Point(122, 203);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(104, 69);
+            this.num8.TabIndex = 13;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = false;
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.AccessibleName = "";
+            this.multiplyButton.BackColor = System.Drawing.Color.MediumPurple;
+            this.multiplyButton.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.multiplyButton.ForeColor = System.Drawing.Color.Black;
+            this.multiplyButton.Location = new System.Drawing.Point(342, 128);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(104, 69);
+            this.multiplyButton.TabIndex = 14;
+            this.multiplyButton.Text = "*";
+            this.multiplyButton.UseVisualStyleBackColor = false;
+            this.multiplyButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // minusButton
+            // 
+            this.minusButton.AccessibleName = "";
+            this.minusButton.BackColor = System.Drawing.Color.MediumPurple;
+            this.minusButton.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.minusButton.ForeColor = System.Drawing.Color.Black;
+            this.minusButton.Location = new System.Drawing.Point(342, 278);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(104, 69);
+            this.minusButton.TabIndex = 15;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = false;
+            this.minusButton.Click += new System.EventHandler(this.minusButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.AccessibleName = "";
+            this.addButton.BackColor = System.Drawing.Color.MediumPurple;
+            this.addButton.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addButton.ForeColor = System.Drawing.Color.Black;
+            this.addButton.Location = new System.Drawing.Point(342, 353);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(104, 69);
+            this.addButton.TabIndex = 16;
+            this.addButton.Text = "+";
+            this.addButton.UseVisualStyleBackColor = false;
+            // 
+            // clearEachNum
+            // 
+            this.clearEachNum.AccessibleName = "";
+            this.clearEachNum.BackColor = System.Drawing.Color.MediumPurple;
+            this.clearEachNum.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.clearEachNum.ForeColor = System.Drawing.Color.Black;
+            this.clearEachNum.Location = new System.Drawing.Point(122, 128);
+            this.clearEachNum.Name = "clearEachNum";
+            this.clearEachNum.Size = new System.Drawing.Size(104, 69);
+            this.clearEachNum.TabIndex = 17;
+            this.clearEachNum.Text = "CE";
+            this.clearEachNum.UseVisualStyleBackColor = false;
+            // 
+            // clearAll
+            // 
+            this.clearAll.AccessibleName = "";
+            this.clearAll.BackColor = System.Drawing.Color.MediumPurple;
+            this.clearAll.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.clearAll.ForeColor = System.Drawing.Color.Black;
+            this.clearAll.Location = new System.Drawing.Point(232, 128);
+            this.clearAll.Name = "clearAll";
+            this.clearAll.Size = new System.Drawing.Size(104, 69);
+            this.clearAll.TabIndex = 18;
+            this.clearAll.Text = "C";
+            this.clearAll.UseVisualStyleBackColor = false;
+            // 
+            // divideButton
+            // 
+            this.divideButton.AccessibleName = "";
+            this.divideButton.BackColor = System.Drawing.Color.MediumPurple;
+            this.divideButton.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.divideButton.ForeColor = System.Drawing.Color.Black;
+            this.divideButton.Location = new System.Drawing.Point(342, 203);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(104, 69);
+            this.divideButton.TabIndex = 19;
+            this.divideButton.Text = "/";
+            this.divideButton.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
+            this.AccessibleDescription = "";
+            this.AccessibleName = "num1";
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumPurple;
             this.BackgroundImage = global::Calculator_Proj_Manalo.Properties.Resources._91a61acbd64fd851f62ec6faea208748;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(704, 521);
+            this.ClientSize = new System.Drawing.Size(458, 508);
+            this.Controls.Add(this.divideButton);
+            this.Controls.Add(this.clearAll);
+            this.Controls.Add(this.clearEachNum);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.equalSign);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.period);
+            this.Controls.Add(this.negate);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.numBox);
+            this.Controls.Add(this.num1);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Imprint MT Shadow", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(2, 4);
             this.Name = "Form1";
-            this.Text = "Purple_Calcu";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.TextBox numBox;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button negate;
+        private System.Windows.Forms.Button period;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button equalSign;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button clearEachNum;
+        private System.Windows.Forms.Button clearAll;
+        private System.Windows.Forms.Button divideButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,39 @@ namespace Calculator_Proj_Manalo
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void period_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minusButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
